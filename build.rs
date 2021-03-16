561dcda8288689f034555bed28aa070f539ee891
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-16 20:48:25 +0100

    chore(build): add custom build configuration
    - Introduce custom configuration for each chapter

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("api/v1/protos/productinfo.proto")?;
+
+    let chap2_iface_files = &["api/v1/protos/product_info.proto"];
+    let dirs = &["."];
+
+    // Chapter 2: Unary gRPC Pattern
+    tonic_build::configure()
+        .compile(chap2_iface_files, dirs)
+        .unwrap();
+    
     Ok(())
 }
