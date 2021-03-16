bd8ef907ce150d18c16127cfb2c8657945c196ac
Shantanoo Desai <shantanoo.desai@gmail.com>
2021-03-16 21:05:29 +0100

    chore(build): add config for chapter 03

--- build.rs
+++ build.rs
@@ -8,5 +8,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .compile(chap2_iface_files, dirs)
         .unwrap();
     
+    // Chapter 3: gRPC Communication Patterns
+    let chap3_iface_files = &["api/v2/protos/order_mgmt.proto"];
+    tonic_build::configure()
+        .compile(chap3_iface_files, dirs)
+        .unwrap();
     Ok(())
 }
